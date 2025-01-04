9f84b2adb2186e240411544fa977c23011fe8734
jackchealdang <jackchealdang@gmail.com>
2025-01-03 22:35:59 -0600

    add shadcn, button, dropdown-menu, & starting dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,9 @@
-import {useEffect, useState} from 'react';
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import Todo from './components/Todo';
+import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
-
-
-  useEffect(() => {
-    fetch('http://localhost:5000/todos')
-    .then((res) => res.json())
-    .then((data) => setTodos(data));
-
-    console.log(todos);
-  }, []);
-
   return (
-    <div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-    </div>
+      <Todo/>
   );
 }
 
