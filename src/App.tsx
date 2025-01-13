cee6499c53dc963a2216a71c7dca8c8293b739da
jackchealdang <jackchealdang@gmail.com>
2025-01-12 21:02:40 -0600

    update routes to match base

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="login" element={<LoginPage/>}/>
+          <Route path="todo-supabase" element={<HomePage />}/>
+          <Route path="todo-supabase/login" element={<LoginPage/>}/>
         </Routes>
       <Toaster />
     </>
